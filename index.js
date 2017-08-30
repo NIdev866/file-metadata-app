b5cd49345484eddd8a5e633830482e661ee7002b
NIdev866 <nidev866@gmail.com>
2017-08-30 12:09:09 +0100

    added server handling

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.write("testtt")
+const express = require("express")
+const app = express()
+
+
+
+require("dotenv").config()
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "static/index.html"))
+})
+
+app.get("*", (req, res)=>{
+  res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000)
