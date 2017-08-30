98eae35ed2c75579d270a08354900920125a63c4
NIdev866 <nidev866@gmail.com>
2017-08-30 12:47:31 +0100

    finished project

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express")
-const app = express()
 const path = require("path")
-
-
+const bodyParser = require('body-parser')
+const multer = require("multer")
+const app = express()
+app.use(bodyParser.json());
 require("dotenv").config()
+
+app.post('/', multer({ dest: './uploads/'}).single('upload'), function(req,res){
+  res.json({
+    fileSize: req.file.size + " bytes"
+  })
+});
 
 app.get("/", (req, res)=>{
   res.sendFile(path.join(__dirname, "static/index.html"))
